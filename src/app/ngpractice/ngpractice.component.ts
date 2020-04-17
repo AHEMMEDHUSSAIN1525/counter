49534024746d601dd86386bed0579cc2b81120d1
Ahemmed <chinturockzz0494@gmail.com>
2020-04-17 20:51:41 +0530

    changed with viewchild

--- src/app/ngpractice/ngpractice.component.ts
+++ src/app/ngpractice/ngpractice.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild,} from '@angular/core';
 
 @Component({
   selector: 'app-ngpractice',
   templateUrl: './ngpractice.component.html',
   styleUrls: ['./ngpractice.component.css']
 })
-export class NgpracticeComponent implements OnInit {
+export class NgpracticeComponent  {
 
-  constructor() { }
+@ViewChild('Input')
+text;
+TextMessage:string='';
+count:number=0
 
-  ngOnInit(): void {
-  }
+ValidInput(){
+  this.count=this.count+1;
+  this.TextMessage="you entered" +" " +this.count +" " +"Times";
+  console.log(this.text.nativeElement.value);
+}
+
+  
 
 }
